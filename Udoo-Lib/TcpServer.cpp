c557dc050c16d516c6ecc7d5daac2766b2c580b8
David Smerkous <smerkousdavid@gmail.com>
2015-11-21 10:29:22 -0700

    initial

--- Udoo-Lib/TcpServer.cpp
+++ Udoo-Lib/TcpServer.cpp
@@ -1,1 +1,41 @@
+#include "Arduino.h"
+#include "Udoo.h"
 
+Udoo builtinTCPS;
+#define wait TcpServer::waiter
+
+void TcpServer::start(int port)
+{
+  builtinTCPS.parseWrite("TCPSERVstart",String(port));
+  wait();
+}
+
+void TcpServer::clientWait(int wait)
+{
+  wait();
+  delay(1);
+  delay(wait);
+}
+
+void TcpServer::send(String toSend)
+{
+  builtinTCPS.parseWrite("TCPSERVsend", toSend);
+  wait();
+}
+
+String TcpServer::recv()
+{
+  builtinTCPS.parseWrite("TCPSERVrecv","");
+  return builtinTCPS.parseRead("TCPSERVrecv");
+}
+
+void TcpServer::close()
+{
+  builtinTCPS.parseWrite("TCPSERVclose","");
+  wait();
+}
+
+void TcpServer::waiter()
+{
+  builtinTCPS.waiting();
+}
