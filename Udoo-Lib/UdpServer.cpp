d847ceb052e3168816852ba56fc94db67ef6fa84
David Smerkous <smerkousdavid@gmail.com>
2015-11-22 11:17:07 -0700

    UDP SERVER HUN

---
+++ Udoo-Lib/UdpServer.cpp
@@ -0,0 +1,34 @@
+#include "Arduino.h"
+#include "Udoo.h"
+
+Udoo builtinUDPS;
+#define wait UdpServer::waiter
+
+void UdpServer::start(int port)
+{
+  builtinUDPS.parseWrite("UDPSERVstart",String(port));
+  wait();
+}
+
+void UdpServer::stop()
+{
+  builtinUDPS.parseWrite("UDPSERVstop","";
+  wait();
+}
+
+void UdpServer::send(String toSend)
+{
+  builtinUDPS.parseWrite("UDPSERVsend", toSend);
+  wait();
+}
+
+String UdpServer::recv()
+{
+  builtinUDPS.parseWrite("UDPSERVrecv","");
+  return builtinUDPS.parseRead("UDPSERVrecv");
+}
+
+void UdpServer::waiter()
+{
+  builtinUDPS.waiting();
+}
