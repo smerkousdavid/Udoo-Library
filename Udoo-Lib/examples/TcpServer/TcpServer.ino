b4d8f84138173c6139aa55448897498e7f17d4bb
David Smerkous <smerkousdavid@gmail.com>
2015-11-23 10:35:28 -0700

    Update TcpServer.ino

--- Udoo-Lib/examples/TcpServer/TcpServer.ino
+++ Udoo-Lib/examples/TcpServer/TcpServer.ino
@@ -1,40 +1,23 @@
-#include <Udoo.h> //Import Library
+#include <Udoo.h>
 
-Udoo udoo; //New Instance
-
-/*Now remember the Tcp server is a lot more complicated then
-The client, While you can have multiple servers, it is extremely
-Unrecommended, If any way possible use the Tcp client for 
-reliability purposes
-
-The reason why is because the Sam is only a single thread and it is a little
-difficult a multithread process on the linux side, since a client is
-single threaded I recommend using the Udoo as a client, Unless you
-develop custom code for a TCP Server, Since i made this library in less
-than a Day it is still under dev
-*/
+Udoo udoo;
+TcpServer server;
 
 void setup() {
     udoo.init(); //Must be called before any udoo commands
-    udoo.tcpServer(8080); //Tell the Linux script to get a Server ready on port 
+    server.start(8080); //Start tcp server
+    //To stop a server use server.stop()
     String ip = udoo.getIp(); //Get the current Linux side Ip address
-    debug(ip); //Debug on the console the current ip
-    if(ip.indexOf("0") == 0) //The script will return 0 if not connected
-    {
-       debug("Not connected to the Internet/Router");
-       while(1); //No point on moving one
-    }
-    
+    debug(ip); //Debug on the console the current ip (Function below)
     
 }
 
 void loop() {
-  udoo.waitForClient(); //Remember to call this to make sure you have a connection
+  server.clientWait(); //Wait until a client connects
   debug("Got Client");
-  debug(udoo.recvFromClient()); //Wait until the Client sends some data
-  delay(500); //Don't rush things
-  udoo.sendToClient("Got it"); //Send back to the client to say A OK
-  udoo.closeTheClient(); //Make sure you close the client connection
+  debug(udoo.recv()); //Recieve from server and print to console
+  udoo.send("Got it bud!"); //Send back to Client
+  udoo.close(); //Close the client connection
 
 }
 
