a576ac5a58488463a538b2a68f7bca4723f02a59
David Smerkous <smerkousdavid@gmail.com>
2015-11-23 10:44:13 -0700

    Update TcpClient.ino

--- Udoo-Lib/examples/TcpClient/TcpClient.ino
+++ Udoo-Lib/examples/TcpClient/TcpClient.ino
@@ -1,18 +1,17 @@
-#include <Udoo.h> //Import the Udoo Library
+#include <Udoo.h>
 
-Udoo udoo; //Create a new Instance
+Udoo udoo;
+TcpClient client;
 
 void setup() {
   udoo.init(); //Must call this before any other udoo commands
-  udoo.tcpClient("192.168.1.250", 8080); //Set Ip and port for TcpClient
+  client.connet("192.168.1.250", 8080); //Set Ip and port for TcpClient
 
 }
 
 void loop() {
-  udoo.clientSend("From Udoo"); //Send String to Server
-  udoo.debug(udoo.clientRecv()); //Since theserial port is used you
-                                 //must use udoo.debug to print information
-                                 //The clientRecv returns a Full String for TCP
-  udoo.tcpClose(); //Yout must close a TCP socket
-  delay(5000); //Remember not to spam the server
+  client.send("From Udoo"); //Send String to Server
+  udoo.debug(client.recv()); //Recieve from server then send to console
+  client.close(); //Close the socket
+  delay(1000);
 }
