e0e15bede42efce3dd17c588248ed3f0633fe6e1
David Smerkous <smerkousdavid@gmail.com>
2015-11-23 10:45:32 -0700

    Update Debug.ino

--- Udoo-Lib/examples/Debug/Debug.ino
+++ Udoo-Lib/examples/Debug/Debug.ino
@@ -5,12 +5,8 @@ Udoo udoo;
 void setup() {
   udoo.init(); //Must call this before any other udoo commands but can be placed after regular commands
   udoo.debug("LINE 1"); //The debug command auto creates new line
-  udoo.debug("Hello World!");
-  for(int a = 0; a < 3; a++)
-  {
-   udoo.debug("Place string here: " + String(a)); //Make sure it's a String
-     //Imaginary a++;                             //Well Strings are easy so that's fun
-  }  //Yaaayy ^
+  udoo.debug("Hello World!\nLine three");//You can also manually insert new line
+  udoo.stop(); //Stop serial line
 }
 
 void loop() {
