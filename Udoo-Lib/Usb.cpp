ac463240ccc81ac9ae9f5afe22edfb4cf4ad7cb2
David Smerkous <smerkousdavid@gmail.com>
2015-11-23 22:08:39 -0700

    Create Usb.cpp

---
+++ Udoo-Lib/Usb.cpp
@@ -0,0 +1,34 @@
+#include "Arduino.h"
+#include "Udoo.h"
+
+Udoo builtinUsb;
+#define wait Usb::waiter
+
+String Usb::read()
+{
+  builtinUsb.parseWrite("SERIALread","");
+  return String(builtinUsb.parseRead("SERIALread"));
+}
+
+void Usb::write(String toWrite)
+{
+  builtinUsb.parseWrite("SERIALwrite", String(toWrite));
+  wait();
+}
+
+void Usb::start(String port, int bit)
+{
+  builtinUsb.parseWrite("SERIALstart", port+"<%=BIT=%>"+String(bit));
+  wait();
+}
+
+String Usb::stop()
+{
+  builtinUsb.parseWrite("SERIALstop", "");
+  wait();
+}
+
+void Usb::waiter()
+{
+  builtinUsb.waiting();
+}
