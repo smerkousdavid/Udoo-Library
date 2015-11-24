44cab6290d1902875e7f7284a87323e24df6e3fb
David Smerkous <smerkousdavid@gmail.com>
2015-11-23 22:51:24 -0700

    Update Usb.ino

--- Udoo-Lib/examples/Usb/Usb.ino
+++ Udoo-Lib/examples/Usb/Usb.ino
@@ -8,6 +8,7 @@ void setup() {
   usb.start("/dev/tty", 115200); //First input is port second it bitrate (Current set won't work)
   usb.send("Hello from udoo Serial to Serial"); //Please don't use newline
   udoo.debug("Got from usb: "+ usb.read()); //wait until readline on usb port(Serial)
+  usb.stop(); //Close the line
 
 }
 
