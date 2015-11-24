9c2eb49a563c70a1bf45dfef8d62d21964d8dd35
smerkoudavid <smerkousdavid@gmail.com>
2015-11-24 05:48:20 +0000

    Usb example

---
+++ Udoo-Lib/examples/Usb/Usb.ino
@@ -0,0 +1,15 @@
+#include <Udoo.h>
+
+Udoo udoo;
+Usb usb;
+
+void setup() {
+  udoo.init(); //Must call this before any other udoo commands
+  usb.start("/dev/tty", 115200); //First input is port second it bitrate (Current set won't work)
+  usb.send("Hello from udoo Serial to Serial"); //Please don't use newline
+  udoo.debug("Got from usb: "+ usb.read()); //wait until readline on usb port(Serial)
+
+}
+
+void loop() {
+}
